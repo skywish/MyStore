1c7bce52515f97f2a1004a845befce8bd8fec643
Skywish <skywish@yeah.net>
2015-04-09 23:39:25 +0800

    ShowGoodDetail

--- app/src/main/java/com/skywish/mystore/adapter/MyGoodAdapter.java
+++ app/src/main/java/com/skywish/mystore/adapter/MyGoodAdapter.java
@@ -5,12 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.skywish.mystore.R;
 import com.skywish.mystore.model.Good;
 
 import java.util.List;
+
+import cn.bmob.v3.listener.DeleteListener;
 
 /**
  * Created by skywish on 2015/4/8.
@@ -18,6 +22,7 @@ import java.util.List;
 public class MyGoodAdapter extends ArrayAdapter<Good>{
 
     private int resourceId;
+    private String objectId;
 
     public MyGoodAdapter(Context context, int textViewResourceId, List<Good> objects) {
         super(context, textViewResourceId, objects);
@@ -35,6 +40,7 @@ public class MyGoodAdapter extends ArrayAdapter<Good>{
             viewHolder.title = (TextView) view.findViewById(R.id.tv_name);
             viewHolder.sales = (TextView) view.findViewById(R.id.tv_sales);
             viewHolder.stock = (TextView) view.findViewById(R.id.tv_stock);
+            viewHolder.deleteBtn = (Button) view.findViewById(R.id.myGoodList_delete);
             view.setTag(viewHolder);
         } else {
             view = convertView;
@@ -43,6 +49,30 @@ public class MyGoodAdapter extends ArrayAdapter<Good>{
         viewHolder.stock.setText(good.getStock()+"");
         viewHolder.sales.setText(good.getSales()+"");
         viewHolder.title.setText(good.getTitle());
+        objectId = good.getObjectId();
+
+        viewHolder.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteItem();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+//                builder.setTitle("确认");
+//                builder.setMessage("确定吗？");
+//                builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        deleteItem();
+//                    }
+//                });
+//                builder.setNegativeButton("否", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
+
+            }
+        });
         return view;
     }
 
@@ -50,5 +80,24 @@ public class MyGoodAdapter extends ArrayAdapter<Good>{
         TextView title;
         TextView sales;
         TextView stock;
+        Button deleteBtn;
+    }
+
+    public void deleteItem() {
+        Good good = new Good();
+        good.setObjectId(objectId);
+        good.delete(getContext(), new DeleteListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(), "删除成功，请刷新",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                Toast.makeText(getContext(), "删除失败，请重试",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
